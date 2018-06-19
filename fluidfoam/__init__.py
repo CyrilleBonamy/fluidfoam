be6f3500492ab10273296eb5aefc9d179ee310b2
jchauchat <none@none>
2018-06-19 20:49:53 +0200

    meshdesign functions added

--- fluidfoam/__init__.py
+++ fluidfoam/__init__.py
@@ -1,4 +1,3 @@
-
 import warnings
 
 from fluidfoam.readof import readscalar, readvector, readtensor
@@ -6,8 +5,7 @@ from fluidfoam.readof import readsymmtensor, readfield, readmesh
 from fluidfoam.readof import typefield
 from fluidfoam.create1dprofile import create1dprofil, read1dprofil
 from fluidfoam.create1dprofile import plot1dprofil
-from fluidfoam.readpostpro import readforce
-
+from fluidfoam.meshdesign import getgz,getdzs
 from fluidfoam._version import __version__
 
 warnings.simplefilter('always', category=DeprecationWarning)
