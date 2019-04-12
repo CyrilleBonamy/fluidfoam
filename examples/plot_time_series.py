f2b4921e9a8e5576fb335ce7fe98eaf22a699556
Antoine Mathieu <antoine.mathieu@univ-grenoble-alpes.fr>
2019-04-12 13:32:26 +0200

    add np.sort in time_series example

--- examples/plot_time_series.py
+++ examples/plot_time_series.py
@@ -9,36 +9,39 @@ This example reads and plots a time series of an OpenFoam scalar field
 # Gets the time directories
 # -------------------------
 #
-# .. note:: Tries if directory is a number and adds it in the time list 
+# .. note:: Tries if directory is a number and adds it in the time array 
 import os
+import numpy as np 
 
 sol = '../output_samples/box/'
 dir_list = os.listdir(sol)
-time_list = []
+time_list = np.empty(0)
 
 for directory in dir_list:
     try:
         float(directory)
-        time_list.append(directory)
+        time_list = np.append(time_list, directory)
     except:
         pass
+time_list = np.sort(time_list)
 
 ###############################################################################
 # Reads a scalar value at a given position for different times
 # ------------------------------------------------------------
 #
 # .. note:: It reads the scalar field p at position 20 and stores it in the
-#           list time_series
+#           numpy array time_series
 
 # import readvector function from fluidfoam package
 from fluidfoam import readscalar
 
 sol = '../output_samples/box/'
 
-time_series = []
+time_series = np.empty(0)
+
 for timename in time_list:
     p = readscalar(sol, timename, 'p')
-    time_series.append(p[20])
+    time_series = np.append(time_series, p[20])
 
 ###############################################################################
 # Now plots the time series
