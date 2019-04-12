426ec9af02a58e6df5ec0c759891db39b1264aec
Cyrille Bonamy <cyrille.bonamy@legi.cnrs.fr>
2019-04-12 17:34:16 +0200

    update plot_contour

--- examples/plot_contour.py
+++ examples/plot_contour.py
@@ -7,8 +7,8 @@ vector field from a structured mesh
 """
 
 ###############################################################################
-# First reads the mesh
-# --------------------
+# First reads the mesh and print the shape/size of the mesh
+# ---------------------------------------------------------
 #
 # .. note:: It reads the mesh coordinates for a structured mesh (argument True)
 #           and stores them in variables x, y and z
@@ -20,6 +20,9 @@ from fluidfoam import readmesh
 sol = '../output_samples/box/'
 
 x, y, z = readmesh(sol, True)
+
+nx, ny, nz = x.shape
+print("Nx = ", nx, "Ny = ", ny, "Nz = ", nz)
 
 ###############################################################################
 # Reads a vector field
@@ -39,12 +42,28 @@ vel = readvector(sol, timename, 'U', True)
 # Now plots the contour of the first velocity component at a given z position
 # ---------------------------------------------------------------------------
 # 
+# .. note:: Here the position z is the middle (// is used to have an integer)
 
 import matplotlib.pyplot as plt
 
 plt.figure()
-plt.contourf(x[:, :, 0], y[:, :, 0], vel[0, :, :, 0])
+plt.contourf(x[:, :, nz//2], y[:, :, nz//2], vel[0, :, :, nz//2])
 
-#Setting axis labels
+# Setting axis labels
 plt.xlabel('x (m)')
 plt.ylabel('y (m)')
+
+###############################################################################
+# Now add on the same plot the velocity vectors
+# ---------------------------------------------
+# 
+
+plt.figure()
+plt.contourf(x[:, :, nz//2], y[:, :, nz//2], vel[0, :, :, nz//2])
+
+# Setting axis labels
+plt.xlabel('x (m)')
+plt.ylabel('y (m)')
+
+plt.quiver(x[:, :, nz//2], y[:, :, nz//2],
+           vel[0, :, :, nz//2], vel[1, :, :, nz//2])
